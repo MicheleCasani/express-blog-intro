f407759e3ba4ddbed1a59672b11119214c174f1d
MicheleCasani <casanimichele@gmail.com>
2025-04-16 14:28:02 +0200

    [UPD] add app.get ('/')  and app.listen

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const app = express();
 // definisco il numero di porta
 const port = 3000;
 
+// definisco la rotta base
+app.get('/', (req, res)=>{
+    let landingPgace = 'Server del mio blog'
+    res.send(landingPgace)
+});
+
+// definisco la seconda rotta 
+
+// dico al server di rimanere in ascolto sulla porta assegnata, ovvero 3000
+app.listen(port, () =>{
+console.log(port);
+});
