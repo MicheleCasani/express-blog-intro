49adda322404752e01a0b28ae7077df0caaaaa32
MicheleCasani <casanimichele@gmail.com>
2025-04-16 14:15:40 +0200

    [UPD] import express

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('ciao')
+// importo express
+const express = require ('express');
+// inizializzo express nella variabile app
+const app = express();
+// definisco il numero di porta
+const port = 3000;
+
